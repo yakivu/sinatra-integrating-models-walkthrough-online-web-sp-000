5ab458fc733b72a24954766a61e42c64659669d1
Adam Walker <adamw207@yahoo.com>
2021-04-06 18:48:25 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,8 @@
 ENV["SINATRA_ENV"] = "test"
 require_relative '../config/environment'
 require 'rack/test'
-
+require 'capybara/rspec'
+require 'capybara/dsl'
 
 RSpec.configure do |config|
   config.include Capybara::DSL

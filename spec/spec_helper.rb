d0f13c7bad479766f9b929141aad59b649aa1ba1
Adam Walker <adamw207@yahoo.com>
2021-04-06 18:42:48 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,3 @@
 
 
 
-ENV["SINATRA_ENV"] = "test"
-require_relative '../config/environment'
-require 'rack/test'
-require 'capybara/rspec'
-require 'capybara/dsl'
-
-
-RSpec.configure do |config|
-@@ -13,4 +15,4 @@ def app
-  Rack::Builder.parse_file('config.ru').first
-end
-
-Capybara.app = app 
-Capybara.app = app
